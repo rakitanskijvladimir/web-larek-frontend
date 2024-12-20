6f714067ffe90ff4e6315d2c08ff0b97caea209a
vladimir <rakitanskijvladimir4@gmail.com>
2024-12-20 13:55:08 +0300

    редакция 20 12 2024

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,68 +1,62 @@
 
+// Интерфейс карточки 
+export interface IProduct {  
+	id: string;           
+	description: string;  
+	image: string;        
+	title: string;        
+	category: string;     
+	price: number | null;        
+}
 
-// Интерфейс модели данных
-interface IProduct { // описание объекта продукт, выводим для отображения на экране
+// Интерфейс корзины
+export interface IProductBasket { 
+    items: string[];
+	total: number;  
+}
+
+// Интерфейс формы отправки 
+export interface IOrderRequest { 
+    payment: PayForm;
+	email: string;
+	phone: string;
+	address: string;
+	items: string[];
+	total: number;      
+}
+// Интерфейс завершенного результата
+export interface IOrderResult {
 	id: string;
-	description: string;
-	image: string;
-	title: string;
-	category: string;
-	price: number;
+	total: number;
 }
 
-interface IGetProductResponse { // ответ от сервера
-	total: number;   // итого сколько пришло товаров
-    items: IProduct[]; // массив товаров
-}
+export type PayForm = 'cash' | 'card';
 
-interface IOrderBody { // параметры запроса postOrder для передачи в body
-    payment: "online" | "offline",
-    email: string,
-    phone: string,
-    address: string,
-    total: number,
-    items: string[]; 
-}
+export type TOrderRequest = Omit<IOrderRequest, 'items' | 'total'>
 
-interface IPostOrderRequest { // ответ от сервера на запрос postOrder
-    id: string,
-    total: number
-}
+export type TOrderFirstForm = Pick<IOrderRequest, 'payment' | 'address'>;
 
-
-// Интерфейс API-клиента
-interface IApiClient { // интерфэйс API клиента
-    getProductList(): Promise<IProduct[]>
-    getProductItem(id: string): Promise<IProduct>
-    postOrder(order: IOrderBody): Promise<IPostOrderRequest>
-}
+export type TOrderSecondForm = Pick<IOrderRequest, 'email' | 'phone'>;
 
 
 
-// Интерфейс базовых классов
-    interface IViewCompinent { // интерфэйс абстрактного класса
-        render(): HTMLElement;
-        update?(): void;
-        mount(parent: HTMLElement): void;
-        unmount(element: HTMLElement): void;
-    }
 
 
-// Интерфейс отображения
-interface IProductView { // интер для отображение элемента на главной странице
-    id: string
-	description: string;
-	image: string;
-	title: string;
-	category: string;
-	price: number;
-}
 
-interface IProductModal extends IProductView { // наследуею от IProductView и добавляю метод
-    onSubmit(id:string): void;
-}
 
-type TProductBasket = Pick<IProductView, 'price' | 'title' | 'id'> // оставляю из IProductView нужные поля
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
